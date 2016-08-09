5445d5d3c83519846865477edb85f3e17c2208db
leaalex <macpro@mac-pro-mac.at.urfu.ru>
2016-08-09 12:33:11 +0500

    Создание всех основных блоков player

---
+++ script.js
@@ -0,0 +1,78 @@
+// Функция получает селектор возвращает массив обектов. В массие может быть один элемент
+
+
+function query(selector){
+    var array = [];
+    var elements = document.querySelectorAll(selector);
+    for (var i = 0; i < elements.length; i++){
+        array.push(elements[i]);
+    };
+    return array;
+}
+// Функция гегенрации ID
+function genID(value){
+    var value = value+"_" || "id"
+        return value + Math.random().toString(16).substr(2, 8).toUpperCase();
+};
+// Функция получение JSON'а по URL и преобрзование его в объект
+function getObjectJSON(urlJSON){
+    var objectJSON = {};
+    var request = new XMLHttpRequest();
+    request.open('GET', urlJSON, false);
+    request.onreadystatechange = function(e) {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                console.log("Загрузка успешно завершена: " , urlJSON);
+                objectJSON = JSON.parse(this.responseText);
+            }
+            else {
+                console.log("Ошибка загрузки: " + urlJSON)
+            }
+        }
+    }
+    request.send(null);
+    return objectJSON;
+};
+// Функция получения свойства каждого из элементов в нутри обекта
+function mediaObjects(objectJSON, type, property){
+    var elements = [];
+    objectJSON[type].forEach(function(element){
+        if(element.type != "SPEAKER_CHANGED"){
+            elements.push(element[property])
+        }
+    });
+    return elements;
+};
+// Функция создания видео/аудео блока
+function createMediaELement(type, name, src){
+    var element = document.createElement(type);
+    element.controls = false;
+    element.preload = "metadata";
+    var source = document.createElement("source");
+    source.src = src + name;
+    source.type = "video/webm";
+    element.appendChild(source);
+    return element;
+};
+// Функция создания HTML элемента
+function createElement(tagName, id, classList, attributes, properties){
+
+  var element = document.createElement(tagName);
+  if (id) {
+    element.id = id;
+  };
+  if (classList) {
+    element.classList = classList;
+  };
+  if (attributes){
+    for (attribute in attributes){
+      element.setAttribute(attribute, attributes[attribute]);
+    };
+  };
+  if (properties){
+    for (propertiy in properties){
+      element[propertiy] =  properties[propertiy];
+    }
+  }
+  return element;
+};
