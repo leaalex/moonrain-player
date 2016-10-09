d78a4fcae632b49e88bd97fc2309da0d48f48872
leaalex <lea_alex@mail.ru>
2016-10-09 21:00:34 +0500

    чистка кода

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ function query(selector){
     var elements = document.querySelectorAll(selector);
     for (var i = 0; i < elements.length; i++){
         array.push(elements[i]);
-    };
+    }
     return array;
 }
 // Функция гегенрации ID
 function genID(value){
-    var value = value+"_" || "id"
+    var value = value+"_" || "id";
         return value + Math.random().toString(16).substr(2, 8).toUpperCase();
-};
+}
 // Функция получение JSON'а по URL и преобрзование его в объект
 function getObjectJSON(urlJSON){
     var objectJSON = {};
@@ -26,23 +26,23 @@ function getObjectJSON(urlJSON){
                 objectJSON = JSON.parse(this.responseText);
             }
             else {
-                console.log("Ошибка загрузки: " + urlJSON)
+                console.log("Ошибка загрузки: " + urlJSON);
             }
         }
-    }
+    };
     request.send(null);
     return objectJSON;
-};
+}
 // Функция получения свойства каждого из элементов в нутри обекта
 function mediaObjects(objectJSON, type, property){
     var elements = [];
     objectJSON[type].forEach(function(element){
         if(element.type != "SPEAKER_CHANGED"){
-            elements.push(element[property])
+            elements.push(element[property]);
         }
     });
     return elements;
-};
+}
 // Функция создания видео/аудео блока
 function createMediaELement(type, name, src){
     var element = document.createElement(type);
@@ -53,48 +53,48 @@ function createMediaELement(type, name, src){
     source.type = "video/webm";
     element.appendChild(source);
     return element;
-};
+}
 // Функция создания HTML элемента
 function createElement(tagName, id, classList, attributes, properties){
 
   var element = document.createElement(tagName);
   if (id) {
     element.id = id;
-  };
+  }
   if (classList) {
     element.classList = classList;
-  };
+  }
   if (attributes){
-    for (attribute in attributes){
+    for (var attribute in attributes){
       element.setAttribute(attribute, attributes[attribute]);
-    };
-  };
+    }
+  }
   if (properties){
-    for (propertiy in properties){
+    for (var propertiy in properties){
       element[propertiy] =  properties[propertiy];
     }
   }
   return element;
-};
+}
 
 function formatTime(time) {
     var hours = (time / 3600) >= 1.0;
     if (hours) {
         var h = Math.floor(time / 3600);
         time = time - h * 3600;
-                    
+
         var m = Math.floor(time / 60);
         var s = Math.floor(time % 60);
-                    
+
         return h.lead0(2)  + ":" + m.lead0(2) + ":" + s.lead0(2);
     } else {
         var m = Math.floor(time / 60);
         var s = Math.floor(time % 60);
-                    
+
         return m.lead0(2) + ":" + s.lead0(2);
     }
 }
-            
+
 Number.prototype.lead0 = function(n) {
     var nz = "" + this;
     while (nz.length < n) {
