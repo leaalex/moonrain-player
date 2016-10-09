76dd23c2e796f457423b6b826c285b0d2d0baa10
leaalex <lea_alex@mail.ru>
2016-10-10 00:58:37 +0500

    новая версия плеера

--- moonrain/player.js
+++ moonrain/player.js
@@ -1,3 +1,39 @@
-function red(){
-    document.querySelector("div").classList.add("red");
+function findPlayer(){
+
+}
+function createPlayer(){
+
+
+}
+function createElement(tagName, id, classList, attributes, properties){
+    var element = document.createElement(tagName);
+    if(classList){
+        element.classList.add(classList);
+    }
+    if(classList){
+        element.id = id;
+    }
+    if (attributes){
+        for(var attribute in attributes){
+            element.setAttribite(attribute, attributes[attribute]);
+        }
+    }
+    if (properties){
+        for (var propertiy in properties){
+            element[propertiy] =  properties[propertiy];
+        }
+    }
+    return element;
+}
+
+function genID(value){
+    var value = value+"_" || "id";
+        return value + Math.random().toString(16).substr(2, 8).toUpperCase();
+}
+
+function createMediaElement(type, name, src){
+    var element = createElement(type, genID(type), 'class_'+type, false, {controls:false, preload:"metadata"});
+    var source = createElement("source", false, false, false, {src:src + name, type: "video/webm"});
+    element.appendChild(source);
+    return element;
 }
